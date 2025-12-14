55a7d69edb435bd001bd1fc1559fd558e25d069c
tkfx-code <tkfx.se@gmail.com>
2025-12-14 16:31:07 +0100

    Added DBContext integration test

---
+++ Test.BookARoom/DBContextTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookARoom.Data;
+using BookARoom.Models;
+using BookARoom.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test.BookARoom
+{
+    public class DBContextTest
+    {
+        private BookingDbContext RealDbContext ()
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Test.BookARoom;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            var options = new DbContextOptionsBuilder<BookingDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            var dbContext = new BookingDbContext(options);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            return dbContext;
+        }
+        [Fact]
+        public async Task PostBooking_RealDB_ShouldSaveAndFetch()
+        {
+            //Arrange
+            using var context = RealDbContext();
+            var repo = new BookingRepo(context);
+
+            var expected = new Booking
+            {
+                RoomId = 999,
+                UserName = "Integration test",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(2)
+            };
+
+            //Act
+            await repo.PostBooking(expected);
+
+            //Assert
+            var actualBooking = await context.Bookings.FindAsync(expected.BookingId);
+            Assert.NotNull(actualBooking);
+            Assert.Equal(expected.RoomId, actualBooking.RoomId);
+            Assert.Equal(expected.UserName, actualBooking.UserName);
+        }
+    }
+}
